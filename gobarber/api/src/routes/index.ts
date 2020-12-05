f45d8ea129dd1bdbfbcce3a43ee71c59732fa259
Matheus Limão Brites <majortheus@gmail.com>
2020-12-04 22:34:21 -0300

    starting gobarber api

---
+++ gobarber/api/src/routes/index.ts
@@ -0,0 +1,9 @@
+import { json, Router } from 'express';
+import appointmentsRouter from './appointments.routes';
+
+const routes = Router();
+
+routes.use(json());
+routes.use('/appointments', appointmentsRouter);
+
+export default routes;
