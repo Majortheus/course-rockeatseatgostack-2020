d4d5fa1c9abfd3953b11e0d6ef5fac945f23d87d
Matheus Limão Brites <majortheus@gmail.com>
2021-05-17 23:05:27 -0300

    app ready for deploy android

--- gobarber/app/src/App.tsx
+++ gobarber/app/src/App.tsx
@@ -1,22 +1,30 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar, View } from 'react-native';
+
+import SlashScreen from 'react-native-splash-screen';
 
 import AppProvider from './hooks';
 
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" translucent />
-    <AppProvider>
-      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-        <Routes />
-      </View>
-    </AppProvider>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    SlashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" translucent />
+      <AppProvider>
+        <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+          <Routes />
+        </View>
+      </AppProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
